ab55d40c044328a0d9d511e8e9cebfbb7dc5743b
DrPing <drping974@proton.me>
2023-11-13 14:13:28 +0700

    Add the paddle to the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::math::vec3;
+use bevy::prelude::*;
+use crate::components::paddle::Paddle;
+
+mod components {
+    pub mod paddle;
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: vec3(0., components::paddle::PADDLE_START_Y, 0.),
+                ..default()
+            },
+            sprite: Sprite {
+                color: components::paddle::PADDLE_COLOR,
+                custom_size: Some(components::paddle::PADDLE_SIZE),
+                ..default()
+            },
+            ..default()
+        },
+        Paddle,
+    ));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9))) // Set the background color
+        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
+        .run();
 }
