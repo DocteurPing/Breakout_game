89034da62a9420d0e697f3842b876a66c653387e
DrPing <drping974@proton.me>
2023-11-13 14:43:46 +0700

    Add ball in the game

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use bevy::math::vec3;
 use bevy::prelude::*;
+use crate::components::ball::*;
 use crate::components::paddle::Paddle;
 use crate::components::paddle::*;
 
 mod components {
     pub mod paddle;
+    pub mod ball;
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asser_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteBundle {
@@ -23,6 +25,24 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Paddle,
+    ));
+    let ball_texture_handle = asser_server.load("textures/ball.png");
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: BALL_STARTING_POSITION,
+                ..default()
+            },
+            sprite: Sprite {
+                color: BALL_COLOR,
+                custom_size: Some(BALL_SIZE),
+                ..default()
+            },
+            texture: ball_texture_handle,
+            ..default()
+        },
+        Ball,
+        Velocity(BALL_SPEED * Vec2 { x: 0.0, y: 0.0 }),
     ));
 }
 
@@ -40,12 +60,19 @@ fn update_paddle(
     }
 }
 
+fn update_ball(mut query: Query<(&Ball, &mut Transform, &mut Velocity)>) {
+    for (_, mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.x * 0.02;
+        transform.translation.y += velocity.y * 0.02;
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9))) // Set the background color
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1))) // Set the background color
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
-        .add_systems(Update, update_paddle)
+        .add_systems(FixedUpdate, (update_paddle, update_ball))
         .run();
 }
