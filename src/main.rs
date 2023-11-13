60949fc15b4ce4b93c874262380fcd9a98e58b21
DrPing <drping974@proton.me>
2023-11-13 14:19:30 +0700

    Make the paddle move

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::math::vec3;
 use bevy::prelude::*;
 use crate::components::paddle::Paddle;
+use crate::components::paddle::*;
 
 mod components {
     pub mod paddle;
@@ -11,12 +12,12 @@ fn setup(mut commands: Commands) {
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: vec3(0., components::paddle::PADDLE_START_Y, 0.),
+                translation: vec3(0., PADDLE_START_Y, 0.),
                 ..default()
             },
             sprite: Sprite {
-                color: components::paddle::PADDLE_COLOR,
-                custom_size: Some(components::paddle::PADDLE_SIZE),
+                color: PADDLE_COLOR,
+                custom_size: Some(PADDLE_SIZE),
                 ..default()
             },
             ..default()
@@ -25,11 +26,26 @@ fn setup(mut commands: Commands) {
     ));
 }
 
+fn update_paddle(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Paddle, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= PADDLE_SPEED * 0.02;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += PADDLE_SPEED * 0.02;
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9))) // Set the background color
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
+        .add_systems(Update, update_paddle)
         .run();
 }
